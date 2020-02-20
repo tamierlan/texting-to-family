484ef58186af350c462552aed72dcd5c54ed3d86
tamerlan ermanbet uulu <tommy-eduardo@tamerlans-MacBook-Pro.local>
2020-02-20 14:09:31 -0500

    first commit

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -5,20 +5,17 @@ import './Join.css';
 
 export default function SignIn() {
   const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
+  const [room, setRoom] = useState('Ermanbetovtar');
 
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
-        <h1 className="heading">Join</h1>
+        <h3 className="heading">кошул Таминин групасына</h3>
         <div>
-          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
-        </div>
-        <div>
-          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
+          <input placeholder="атынызды жаз" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
         </div>
         <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-          <button className={'button mt-20'} type="submit">Sign In</button>
+          <button className={'button mt-20'} type="submit">баскычты бас</button>
         </Link>
       </div>
     </div>
